5a61e8c6a6abe79e6645378176b8f417ee764f56
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-05-05 00:30:48 -0400

    setuptools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+  name = 'pylhe',
+  version = '0.0.1',
+  description = 'small package to get structured data out of Les Houches Event files',
+  author = 'Lukas Heinrich',
+  author_email = 'lukas.heinrich@cern.com',
+  packages = find_packages(),
+  install_requires = [
+    'networkx',
+    'tex2pix',
+    'pypdt'
+  ]
+)
