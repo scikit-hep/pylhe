76f23021f1ad24620163c4e141840ca61078d8cb
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-06-24 16:10:31 -0400

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
   name = 'pylhe',
-  version = '0.0.2',
+  version = '0.0.3',
   description = 'small package to get structured data out of Les Houches Event files',
   author = 'Lukas Heinrich',
   author_email = 'lukas.heinrich@cern.com',
