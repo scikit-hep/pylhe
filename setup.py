03f2f580448a21028926dafef3e3ae90d174dd28
Matthew Feckert <matthew.feickert@cern.ch>
2019-11-08 14:35:18 +1030

    Go Black

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 extras_require = {
-    'develop': [
-        'pre-commit',
-        'black;python_version>="3.6"',  # Black is Python3 only
-        'twine',
+    "develop": [
+        "pre-commit",
+        "black;python_version>='3.6'",  # Black is Python3 only
+        "twine",
     ],
 }
-extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
+extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
-  name = 'pylhe',
-  version = '0.0.4',
-  description = 'small package to get structured data out of Les Houches Event files',
-  author = 'Lukas Heinrich',
-  author_email = 'lukas.heinrich@cern.ch',
-  packages = find_packages(),
-  install_requires = [
-    'networkx',
-    'tex2pix',
-    'pypdt'
-  ],
+    name="pylhe",
+    version="0.0.4",
+    description="small package to get structured data out of Les Houches Event files",
+    author="Lukas Heinrich",
+    author_email="lukas.heinrich@cern.ch",
+    packages=find_packages(),
+    install_requires=["networkx", "tex2pix", "pypdt"],
     extras_require=extras_require,
 )
