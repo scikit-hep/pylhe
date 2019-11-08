54218d52dd025df8efa3d30af51c43459bb259e6
Matthew Feckert <matthew.feickert@cern.ch>
2019-11-08 11:44:30 +1030

    Make project Black compatible
    * Add 'develop' and 'complete' install options for Black
    * Add Git pre-commit hook YAML
    * Add .gitignore
    * Add Black code style badge to README

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 [![DOI](https://zenodo.org/badge/34966492.svg)](https://zenodo.org/badge/latestdoi/34966492)
 
 [![PyPI version](https://badge.fury.io/py/pylhe.svg)](https://badge.fury.io/py/pylhe)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
 Small and thin Python interface to read [Les Houches Event (LHE)](https://inspirehep.net/record/725284) files
--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup,find_packages
 
+extras_require = {
+    'develop': [
+        'pre-commit',
+        'black;python_version>="3.6"',  # Black is Python3 only
+        'twine',
+    ],
+}
+extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
+
 setup(
   name = 'pylhe',
   version = '0.0.4',
@@ -11,5 +20,6 @@ setup(
     'networkx',
     'tex2pix',
     'pypdt'
-  ]
+  ],
+    extras_require=extras_require,
 )
