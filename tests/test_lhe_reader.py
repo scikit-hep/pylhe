85dfaab81eaee365753acd7dda44ca6567d6c133
Johannes Schumann <johannes.schumann@fau.de>
2020-05-19 19:19:39 +0200

    Black

--- tests/test_lhe_reader.py
+++ tests/test_lhe_reader.py
@@ -5,21 +5,21 @@ from os.path import dirname, join
 
 TEST_FILE = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
 
+
 def test_event_count():
     assert pylhe.readNumEvents(TEST_FILE) == 791
 
 
 def test_lhe_init():
     init_data = pylhe.readLHEInit(TEST_FILE)
-    init_info = init_data['initInfo']
-    assert init_info['beamA'] == pytest.approx(1.0)
-    assert init_info['beamB'] == pytest.approx(2.0)
-    assert init_info['energyA'] == pytest.approx(1.234567)
-    assert init_info['energyB'] == pytest.approx(2.345678)
-    assert init_info['PDFgroupA'] == pytest.approx(3.0)
-    assert init_info['PDFgroupB'] == pytest.approx(4.0)
-    assert init_info['PDFsetA'] == pytest.approx(5.0)
-    assert init_info['PDFsetB'] == pytest.approx(6.0)
-    assert init_info['weightingStrategy'] == pytest.approx(7.0)
-    assert init_info['numProcesses'] == pytest.approx(8.0)
-
+    init_info = init_data["initInfo"]
+    assert init_info["beamA"] == pytest.approx(1.0)
+    assert init_info["beamB"] == pytest.approx(2.0)
+    assert init_info["energyA"] == pytest.approx(1.234567)
+    assert init_info["energyB"] == pytest.approx(2.345678)
+    assert init_info["PDFgroupA"] == pytest.approx(3.0)
+    assert init_info["PDFgroupB"] == pytest.approx(4.0)
+    assert init_info["PDFsetA"] == pytest.approx(5.0)
+    assert init_info["PDFsetB"] == pytest.approx(6.0)
+    assert init_info["weightingStrategy"] == pytest.approx(7.0)
+    assert init_info["numProcesses"] == pytest.approx(8.0)
