9304b4c2c19c8ad81095199fa88c8142f8e06104
Matthew Feickert <matthew.feickert@cern.ch>
2020-05-30 04:38:36 -0500

    chore: Migrate metadata and setup options to setup.cfg (#34)
    * Move PyPI metadata from setup.py to setup.cfg
    * Move setup options from setup.py to setup.cfg
    * Move flake8 option from .flake8 to setup.cfg
    * Remove now unused pathlib.Path and setuptools.find_packages imports
    * Add classifiers to include development status, license, intended audience, and topic
    * Add project URLs
    * Add README as project description for PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 extras_require = {
     "test": [
@@ -17,16 +17,6 @@ extras_require["develop"] = sorted(
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
-    name="pylhe",
-    version="0.0.5",
-    description="small package to get structured data out of Les Houches Event files",
-    author="Lukas Heinrich",
-    author_email="lukas.heinrich@cern.ch",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    include_package_data=True,
-    install_requires=["networkx", "tex2pix", "pypdt"],
     extras_require=extras_require,
-    dependency_links=[],
     use_scm_version=lambda: {"local_scheme": lambda version: ""},
 )
