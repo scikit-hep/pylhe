fd9fdda3dd9492c8a1bd41b5401c70194f942fff
Matthew Feickert <matthew.feickert@cern.ch>
2020-05-29 22:27:01 -0500

    Migrate metadata and options to setup.cfg

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from setuptools import setup, find_packages
-from pathlib import Path
-
-this_directory = Path(__file__).parent.resolve()
-with open(Path(this_directory).joinpath("README.md"), encoding="utf-8") as readme_md:
-    long_description = readme_md.read()
+from setuptools import setup
 
 extras_require = {
     "test": [
@@ -22,36 +17,6 @@ extras_require["develop"] = sorted(
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
-    name="pylhe",
-    version="0.0.5",
-    description="small package to get structured data out of Les Houches Event files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/scikit-hep/pylhe",
-    project_urls={
-        "Source": "https://github.com/scikit-hep/pylhe",
-        "Tracker": "https://github.com/scikit-hep/pylhe/issues",
-    },
-    author="Lukas Heinrich",
-    author_email="lukas.heinrich@cern.ch",
-    license="Apache",
-    keywords="physics lhe",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    include_package_data=True,
-    install_requires=["networkx", "tex2pix", "pypdt"],
     extras_require=extras_require,
-    dependency_links=[],
     use_scm_version=lambda: {"local_scheme": lambda version: ""},
 )
