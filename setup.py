691ade510c47ad0024742b7ef657b7be818d39a8
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-28 16:24:54 -0500

    Use bump2version v1.X

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ extras_require = {
         "pytest-cov>=2.5.1",
         "scikit-hep-testdata>=0.3.1",
         "pydocstyle",
-        "check-manifest",
         "flake8",
     ],
 }
 extras_require["lint"] = sorted(set(["pyflakes", "black;python_version>='3.6'"]))
 extras_require["develop"] = sorted(
-    set(extras_require["test"] + ["pre-commit", "check-manifest", "twine"])
+    set(
+        extras_require["test"]
+        + ["pre-commit", "check-manifest", "bump2version~=1.0", "twine"]
+    )
 )
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
