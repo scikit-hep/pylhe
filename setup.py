20ebb26edc02654850d6ca8291cd876372d2e188
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-28 16:37:12 -0500

    Remove pyflakes from lint extra

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 extras_require = {
     "test": ["pytest", "pytest-cov>=2.5.1", "scikit-hep-testdata>=0.3.1", "pydocstyle"],
 }
-extras_require["lint"] = sorted(
-    set(["flake8", "pyflakes", "black;python_version>='3.6'"])
-)
+extras_require["lint"] = sorted(set(["flake8", "black;python_version>='3.6'"]))
 extras_require["develop"] = sorted(
     set(
         extras_require["test"]
