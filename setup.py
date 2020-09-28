2a7f8cf9c30ebad1eb620c1fb3d4bce7558ee16b
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-28 16:34:16 -0500

    chore: Migrate to bump2version (#41)
    * Add bump2version to the develop extra
       - bumpversion is no longer maintained and bump2version is the recommended fork
       - Use bump2version v1.X series
    * Migrate flake8 to the lint extra

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup
 
 extras_require = {
-    "test": [
-        "pytest",
-        "pytest-cov>=2.5.1",
-        "scikit-hep-testdata>=0.3.1",
-        "pydocstyle",
-        "check-manifest",
-        "flake8",
-    ],
+    "test": ["pytest", "pytest-cov>=2.5.1", "scikit-hep-testdata>=0.3.1", "pydocstyle"],
 }
-extras_require["lint"] = sorted(set(["pyflakes", "black;python_version>='3.6'"]))
+extras_require["lint"] = sorted(
+    set(["flake8", "pyflakes", "black;python_version>='3.6'"])
+)
 extras_require["develop"] = sorted(
-    set(extras_require["test"] + ["pre-commit", "check-manifest", "twine"])
+    set(
+        extras_require["test"]
+        + ["pre-commit", "check-manifest", "bump2version~=1.0", "twine"]
+    )
 )
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
