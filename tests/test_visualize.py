faca41b1563f92d0b596eeba92bc2162e386907a
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-04 16:28:48 -0600

    feat: Drop Python 2.7 support (#51)
    * Drop support of Python 2.7 and require Python 3.6 or newer
       - Remove Python 2 PyPI metadata
       - Drop testing of Python 2.7
    * Remove Python 2.7 specific stopgap fixes

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -8,8 +8,6 @@ def test_visualize(tmpdir):
     events = pylhe.readLHEWithAttributes(lhe_file)
     start_event = 1
     stop_event = 2
-    # TODO: Use f-strings once Python 2 dropped
-    # TODO: Remove explicit str wrapping once Python 2 dropped
-    filename = str(tmpdir.join("event{}.pdf".format(start_event)))
+    filename = tmpdir.join(f"event{start_event}.pdf")
     for idx, event in enumerate(itertools.islice(events, start_event, stop_event)):
         pylhe.visualize(event, filename)
