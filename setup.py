430b9ba54d4cf252e225d4fa29b054df42e3758a
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-04 14:09:37 -0600

    Add viz extra

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
-extras_require = {
-    "test": ["pytest", "pytest-cov>=2.5.1", "scikit-hep-testdata>=0.3.1", "pydocstyle"],
-}
+extras_require = {}
 extras_require["lint"] = sorted(set(["flake8", "black;python_version>='3.6'"]))
+extras_require["viz"] = sorted(set(["pydot", "dot2tex"]))
+extras_require["test"] = sorted(
+    set(
+        extras_require["lint"]
+        + ["pytest", "pytest-cov>=2.5.1", "scikit-hep-testdata>=0.3.1", "pydocstyle"]
+    )
+)
 extras_require["develop"] = sorted(
     set(
         extras_require["test"]
