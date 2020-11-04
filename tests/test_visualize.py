0d525ebd5884e53d81b6b291aa1d77bd8b2e2b9c
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-04 14:34:34 -0600

    use tmpdir fixture

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -3,10 +3,10 @@ import skhep_testdata
 import itertools
 
 
-def test_visualize():
+def test_visualize(tmpdir):
     lhe_file = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
     events = pylhe.readLHEWithAttributes(lhe_file)
     start_event = 1
     stop_event = 2
     for idx, event in enumerate(itertools.islice(events, start_event, stop_event)):
-        pylhe.visualize(event, f"event{idx+start_event}.pdf")
+        pylhe.visualize(event, tmpdir.join(f"event{idx+start_event}.pdf"))
