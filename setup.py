13a2857e319cd4fdd47ed21f7b2ed34d003fd3f0
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-16 23:30:34 -0600

    Apply to viz extra

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
 extras_require = {}
-extras_require["lint"] = sorted({"flake8", "black;python_version>='3.6'"})
+extras_require["lint"] = sorted({"flake8", "black"})
 # c.f. https://networkx.org/documentation/stable/install.html#optional-packages
-extras_require["viz"] = sorted({"pydot", "dot2tex"})
+extras_require["viz"] = sorted({"pydot~=1.4", "dot2tex~=2.11"})
 extras_require["test"] = sorted(
     set(
         extras_require["viz"]
-        + ["pytest", "pytest-cov>=2.5.1", "scikit-hep-testdata>=0.3.1", "pydocstyle"]
+        + [
+            "pytest~=6.0",
+            "pytest-cov>=2.5.1",
+            "scikit-hep-testdata>=0.3.1",
+            "pydocstyle",
+        ]
     )
 )
 extras_require["develop"] = sorted(
