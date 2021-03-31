a6315b9ee5d5192776f0996fea86e06feaf6d941
Matthew Feickert <matthew.feickert@cern.ch>
2021-03-31 14:44:30 -0500

    test with valid file and with invalid

--- tests/test_lhe_reader.py
+++ tests/test_lhe_reader.py
@@ -1,5 +1,3 @@
-import gzip
-
 import pytest
 import skhep_testdata
 
@@ -8,13 +6,13 @@ import pylhe
 TEST_FILE = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
 
 
-def test_gzip_open_fail(tmpdir):
+def test_gzip_open(tmpdir):
+    tmp_path = tmpdir.join("notrealfile.lhe")
+    assert pylhe._open_gzip_file(tmp_path) == tmp_path
+
     tmp_path = tmpdir.join("notrealfile.lhe.gz")
     tmp_path.write("")
-    print("\n\n")
-    print(tmp_path)
-    print("\n\n")
-    with pytest.raises(OSError, gzip.BadGzipFile):
+    with pytest.raises(OSError):
         pylhe._open_gzip_file(tmp_path)
 
 
