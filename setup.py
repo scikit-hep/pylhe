6dcbd504401488e443dd23834b18c782f0932a0a
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2022-04-20 18:56:00 +0200

    Replace dependency on networkx with that on graphviz

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ from setuptools import setup
 
 extras_require = {}
 extras_require["lint"] = sorted({"flake8", "black"})
-# c.f. https://networkx.org/documentation/stable/install.html#optional-packages
-extras_require["viz"] = sorted({"pydot~=1.4", "dot2tex~=2.11"})
 extras_require["test"] = sorted(
     set(
-        extras_require["viz"]
-        + [
+        [
             "pytest~=6.0",
             "pytest-cov>=2.5.1",
             "scikit-hep-testdata>=0.3.1",
