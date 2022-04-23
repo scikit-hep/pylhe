5f2822716c671f24d279c55c12c7f66f389b1905
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2022-04-23 12:18:09 +0200

    Add a test

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,15 +1,28 @@
 import itertools
+import graphviz
 
 import skhep_testdata
 
 import pylhe
 
 
+TEST_FILE = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
+
+
 def test_visualize(tmpdir):
-    lhe_file = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
-    events = pylhe.read_lhe_with_attributes(lhe_file)
+    events = pylhe.read_lhe_with_attributes(TEST_FILE)
     start_event = 1
     stop_event = 2
     filename = tmpdir.join(f"event{start_event}.pdf")
     for idx, event in enumerate(itertools.islice(events, start_event, stop_event)):
         pylhe.visualize(event, filename)
+
+
+def test_LHEEvent_graph():
+    events = pylhe.read_lhe(TEST_FILE)
+    # Get the first event
+    e = next(events)
+
+    assert isinstance(e.graph, graphviz.dot.Digraph)
+    # ... it contains 5 pions
+    assert e.graph.source.count("&#x03c0;") == 5
