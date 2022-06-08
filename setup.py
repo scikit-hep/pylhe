b481219c89f244460e16b27dfd160a258d7d4b2b
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2022-06-08 16:38:27 +0200

    Remove dependency on networkx

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 extras_require = {}
 extras_require["lint"] = sorted({"flake8", "black"})
-# c.f. https://networkx.org/documentation/stable/install.html#optional-packages
 extras_require["viz"] = sorted({"pydot~=1.4", "dot2tex~=2.11"})
 extras_require["test"] = sorted(
     set(
