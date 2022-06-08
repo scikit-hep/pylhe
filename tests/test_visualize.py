27bf5466ebcec020ccf827b9b6781c89cf7dc486
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2022-06-08 17:32:15 +0200

    Adapt tests accordingly

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -5,17 +5,6 @@ import skhep_testdata
 import pylhe
 
 
-def test_visualize(tmpdir):
-    lhe_file = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
-    events = pylhe.read_lhe_with_attributes(lhe_file)
-
-    start_event = 1
-    stop_event = 2
-    filename = tmpdir.join(f"event{start_event}.pdf")
-    for event in itertools.islice(events, start_event, stop_event):
-        pylhe.visualize(event, filename)
-
-
 def test_LHEEvent_graph():
     lhe_file = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
     events = pylhe.read_lhe_with_attributes(lhe_file)
