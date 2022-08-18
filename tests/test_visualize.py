2c37af784231d618f05b0fe60d2a3909e65b5e0c
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2022-08-18 16:16:09 +0200

    Add test for visualisation via graph.render

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,9 +1,10 @@
+import itertools
 import skhep_testdata
 
 import pylhe
 
 
-def test_LHEEvent_graph():
+def test_LHEEvent_graph_source():
     lhe_file = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
     events = pylhe.read_lhe_with_attributes(lhe_file)
 
@@ -13,3 +14,11 @@ def test_LHEEvent_graph():
     # pi unicode charecter is '&#x03c0;'
     assert event.graph.source.count("&#x03c0;") == 8
     assert "<td>p</td>" in event.graph.source
+
+
+def test_LHEEvent_graph_render():
+    lhe_file = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
+    events = pylhe.read_lhe_with_attributes(lhe_file)
+
+    event = next(itertools.islice(events, 1, 2))
+    event.graph.render(filename="test_event1", format="pdf", cleanup=True)
