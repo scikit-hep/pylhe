6971374b99c3c9f0e95b05446d30a6eeeefc5272
Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
2023-01-24 15:41:00 -0600

    fix: Update KeyError to new exception of DirectionalMap (#180)
    * update KeyError to new exception of DirectionalMap
    
    * draft test for generating a graph
    
    this probably will fail since the test file referenced is not in the
    skhep_testdata module as of yet; however, I think it is still a good
    starting point
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * need to limit number to node tag
    
    there seems to be other substrings matching that number as part of the
    digraph source, so we are following the proton example from the earlier
    test
    
    * Update import statement
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Eduardo Rodrigues <eduardo.rodrigues@cern.ch>

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -17,6 +17,18 @@ def test_LHEEvent_graph_source():
     assert "<td>p</td>" in event.graph.source
 
 
+def test_LHEEvent_graph_source_nonstandard_pdg():
+    lhe_file = skhep_testdata.data_path("pylhe-testfile-pr180.lhe")
+    events = pylhe.read_lhe_with_attributes(lhe_file)
+
+    # Get the first event
+    event = next(events)
+    # building the graph should succeed even though there is
+    # a non-standard PDG ID 1023 and the name in the graph
+    # source should just be the ID number itself
+    assert event.graph.source.count("<td>1023</td>") == 1
+
+
 def test_LHEEvent_graph_render():
     lhe_file = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
     events = pylhe.read_lhe_with_attributes(lhe_file)
