fc9c6dc43d5ec15c83247b85b90dc27e89add16a
Iason Krommydas <iason.krom@gmail.com>
2025-06-19 17:24:40 +0300

    more and more readable testing

--- tests/test_awkward.py
+++ tests/test_awkward.py
@@ -9,14 +9,24 @@ TEST_FILE_WITH_WEIGHTS = skhep_testdata.data_path("pylhe-testlhef3.lhe")
 def test_to_awkward():
     arr = pylhe.to_awkward(pylhe.read_lhe_with_attributes(TEST_FILE_WITHOUT_WEIGHTS))
     assert len(arr) == 791
-    assert len(arr.eventinfo.nparticles) == len(arr.particles)
+    assert len(arr) == len(arr.particles)
+    assert len(arr) == len(arr.eventinfo)
+    for field in arr.particles.fields:
+        assert len(arr) == len(arr.particles[field])
+    for field in arr.eventinfo.fields:
+        assert len(arr) == len(arr.eventinfo[field])
     assert "weights" not in arr.fields
 
     arr = pylhe.to_awkward(pylhe.read_lhe_with_attributes(TEST_FILE_WITH_WEIGHTS))
     assert len(arr) == 59
-    assert len(arr.eventinfo.nparticles) == len(arr.particles)
-    assert len(arr.eventinfo.nparticles) == len(arr.weights)
+    assert len(arr) == len(arr.particles)
+    assert len(arr) == len(arr.eventinfo)
+    for field in arr.particles.fields:
+        assert len(arr) == len(arr.particles[field])
+    for field in arr.eventinfo.fields:
+        assert len(arr) == len(arr.eventinfo[field])
     assert "weights" in arr.fields
+    assert len(arr) == len(arr.weights)
     assert arr.weights.fields == [
         "1001",
         "1002",
@@ -28,6 +38,8 @@ def test_to_awkward():
         "1008",
         "1009",
     ]
+    for field in arr.weights.fields:
+        assert len(arr) == len(arr.weights[field])
 
 
 def test_awkward_registration():
