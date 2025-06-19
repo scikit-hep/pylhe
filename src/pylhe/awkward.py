49516c32a72d9282a66dd7404da478dd917b4618
Iason Krommydas <iason.krom@gmail.com>
2025-06-19 19:17:14 +0300

    feat: add `weights` to the awkward array if they are present (#294)
    * add weights to to_awkward if present
    
    * more and more readable testing
    
    * improve to_awkward docstring
    
    * dev comments for behaviors
    
    * fix typos
    
    * add explicit weights value tests for awkward
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Alexander Puck Neuwirth <APN-Pucky@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/pylhe/awkward.py
+++ src/pylhe/awkward.py
@@ -9,7 +9,19 @@ def __dir__():
 
 
 def to_awkward(event_iterable):
-    """Convert iterable of LHEEvent instances to Awkward-Array."""
+    """Convert an iterable of LHEEvent instances to an Awkward-Array.
+
+    Uses Awkward's ArrayBuilder to construct the array by iterating over the events.
+    The events_iterable should yield instances of LHEEvent.
+    This is typically created by one of the reading functions pylhe provides like
+    pylhe.read_lhe(filepath), pylhe.read_lhe_with_attributes(filepath), or pylhe.read_lhe_file(filepath).events.
+
+    Args:
+        event_iterable (iterable): An iterable of LHEEvent instances.
+
+    Returns:
+        awkward.Array: An Awkward array of all the events.
+    """
 
     builder = ak.ArrayBuilder()
     for event in event_iterable:
@@ -18,6 +30,11 @@ def to_awkward(event_iterable):
             with builder.record(name="EventInfo"):
                 for fname in event.eventinfo.fieldnames:
                     builder.field(fname).real(getattr(event.eventinfo, fname))
+            if (event.weights is not None) and (event.weights != {}):
+                builder.field("weights")
+                with builder.record(name="Weights"):
+                    for label, w in event.weights.items():
+                        builder.field(label).real(w)
             builder.field("particles")
             with builder.list():
                 for particle in event.particles:
@@ -38,6 +55,7 @@ def to_awkward(event_iterable):
     return builder.snapshot()  # awkward array
 
 
+# Used to register Awkward behaviors
 class Particle:
     pass
 
@@ -50,8 +68,15 @@ class EventInfo:
     pass
 
 
+class Weights:
+    pass
+
+
 # Register Awkward behaviors
+# See https://awkward-array.org/doc/main/reference/generated/ak.mixin_class.html
+# and https://awkward-array.org/doc/main/reference/ak.behavior.html#mixin-decorators
 vector.register_awkward()
 ak.mixin_class(ak.behavior)(Particle)
 ak.mixin_class(ak.behavior)(Event)
 ak.mixin_class(ak.behavior)(EventInfo)
+ak.mixin_class(ak.behavior)(Weights)
