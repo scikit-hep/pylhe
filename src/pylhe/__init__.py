50577cfde1e70cb346968881ead6ed21617a7673
Alexander Puck Neuwirth <APN-Pucky@users.noreply.github.com>
2025-10-08 11:03:48 +0200

    Update src/pylhe/__init__.py
    Co-authored-by: Eduardo Rodrigues <eduardo.rodrigues@cern.ch>

--- src/pylhe/__init__.py
+++ src/pylhe/__init__.py
@@ -214,7 +214,7 @@ class LHEEventInfo:
 
     @property
     def fieldnames(self):
-        """fieldnames backwards compatibility."""
+        """Return the fieldnames. For backward compatibility with versions < 1.0.0."""
         return [f.name for f in fields(self)]
 
 
