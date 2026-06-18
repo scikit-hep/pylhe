1013b919b7764b5fb957da185a5c7424953956d9
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2026-06-18 15:48:50 +0200

    factor out formating to allow user modifications

--- src/pylhe/__init__.py
+++ src/pylhe/__init__.py
@@ -33,15 +33,17 @@ __all__ = [
     "LHEEvent",
     "LHEEventInfo",
     "LHEFile",
-    "LHEFormat",
+    "LHEFileFormat",
     "LHEGenerator",
     "LHEHeader",
     "LHEInit",
     "LHEInitInfo",
     "LHEInitRWGTWeight",
     "LHEInitRWGTWeightGroup",
+    "LHEOuputFormat",
     "LHEParticle",
     "LHEProcInfo",
+    "LHEWeightFormat",
     "__version__",
     "to_awkward",
 ]
@@ -57,12 +59,37 @@ _PDGID2LaTeXNameMap, _ = DirectionalMaps("PDGID", "LATEXNAME", converters=(str, 
 PathLike = Union[str, bytes, os.PathLike[str], os.PathLike[bytes]]
 
 
-class LHEFormat(enum.Enum):
+class LHEWeightFormat(enum.Enum):
     """Selects how event weights are serialized in LHE output."""
 
     RWGT = "rwgt"  # <rwgt><wgt id=...>...</wgt></rwgt> block
     WEIGHTS = "weights"  # <weights>...</weights> block
     NONE = "none"  # no weight block emitted
+
+
+class LHEFileFormat(enum.Enum):
+    PLAIN = "plain"
+    GZIP = "gzip"
+    # HDF5 = "hdf5" # TODO
+
+
+@dataclass(frozen=True, slots=True)
+class LHEOutputFormat:
+    """Future proof, extensible output format"""
+
+    # version: LHEVersion = LHEVersion.V3 #TODO
+    indent: str = "  "
+    weights: LHEWeightFormat = LHEWeightFormat.RWGT
+    file: LHEFileFormat = LHEFileFormat.PLAIN
+    # compression: Compression = Compression.NONE
+
+    eventinfo: str = "{nparticles:3d} {pid:6d} {weight: 15.10e} {scale: 15.10e} {aqed: 15.10e} {aqcd: 15.10e}"
+    particle: str = "{id:5d} {status:3d} {mother1:3d} {mother2:3d} {color1:3d} {color2:3d} {px: 15.8e} {py: 15.8e} {pz: 15.8e} {e: 15.8e} {m: 15.8e} {lifetime: 10.4e} {spin: 10.4e}"
+    initinfo: str = " {beamA: 6d} {beamB: 6d} {energyA: 14.7e} {energyB: 14.7e} {PDFgroupA: 5d} {PDFgroupB: 5d} {PDFsetA: 5d} {PDFsetB: 5d} {weightingStrategy: 5d} {numProcesses: 5d}"
+    procinfo: str = "{xSection: 14.7e} {error: 14.7e} {unitWeight: 14.7e} {procId: 5d}"
+
+
+default_format = LHEOutputFormat()
 
 
 class Writeable(Protocol):
@@ -112,14 +139,21 @@ class LHEEventInfo:
     aqcd: float
     """QCD coupling constant alpha_QCD"""
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """
         Return the event info as a string in LHE format.
 
         Returns:
             str: The event info as a string in LHE format.
         """
-        return f"{self.nparticles:3d} {self.pid:6d} {self.weight: 15.10e} {self.scale: 15.10e} {self.aqed: 15.10e} {self.aqcd: 15.10e}"
+        return lheformat.eventinfo.format(
+            nparitcles=self.nparticles,
+            pid=self.pid,
+            weight=self.weight,
+            scale=self.scale,
+            aqed=self.aqed,
+            aqcd=self.aqcd,
+        )
 
     @classmethod
     def fromstring(cls, string: str) -> "LHEEventInfo":
@@ -219,14 +253,28 @@ class LHEParticle:
             spin=float(values[12]),
         )
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """
         Return the particle as a string in LHE format.
 
         Returns:
             str: The particle as a string in LHE format.
         """
-        return f"{self.id:5d} {self.status:3d} {self.mother1:3d} {self.mother2:3d} {self.color1:3d} {self.color2:3d} {self.px: 15.8e} {self.py: 15.8e} {self.pz: 15.8e} {self.e: 15.8e} {self.m: 15.8e} {self.lifetime: 10.4e} {self.spin: 10.4e}"
+        return lheformat.particle.format(
+            id=self.id,
+            status=self.status,
+            mother1=self.mother1,
+            mother2=self.mother2,
+            color1=self.color1,
+            color2=self.color2,
+            px=self.px,
+            py=self.py,
+            pz=self.pz,
+            e=self.e,
+            m=self.m,
+            lifetime=self.lifetime,
+            spin=self.spin,
+        )
 
     def mothers(self) -> list["LHEParticle"]:
         """
@@ -252,8 +300,8 @@ class LHEParticle:
         ]
 
 
-def _indent(root: ET.Element, level: int = 2) -> None:
-    ET.indent(root, space=" " * level)
+def _indent(root: ET.Element, lheformat: LHEOutputFormat = default_format) -> None:
+    ET.indent(root, space=lheformat.indent)
     root.tail = "\n"
 
 
@@ -282,14 +330,25 @@ class LHEInitInfo:
     numProcesses: int
     """Number of processes"""
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """
         Return the init info block as a string in LHE format.
 
         Returns:
             str: The init info block as a string in LHE format.
         """
-        return f" {self.beamA: 6d} {self.beamB: 6d} {self.energyA: 14.7e} {self.energyB: 14.7e} {self.PDFgroupA: 5d} {self.PDFgroupB: 5d} {self.PDFsetA: 5d} {self.PDFsetB: 5d} {self.weightingStrategy: 5d} {self.numProcesses: 5d}"
+        return lheformat.initinfo.format(
+            beamA=self.beamA,
+            beamB=self.beamB,
+            energyA=self.energyA,
+            energyB=self.energyB,
+            PDFgroupA=self.PDFgroupA,
+            PDFgroupB=self.PDFgroupB,
+            PDFsetA=self.PDFsetA,
+            PDFsetB=self.PDFsetB,
+            weightingStrategy=self.weightingStrategy,
+            numProcesses=self.numProcesses,
+        )
 
     @classmethod
     def fromstring(cls, string: str) -> "LHEInitInfo":
@@ -324,14 +383,19 @@ class LHEProcInfo:
     procId: int
     """Process ID"""
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """
         Return the process info block as a string in LHE format.
 
         Returns:
             str: The process info block as a string in LHE format.
         """
-        return f"{self.xSection: 14.7e} {self.error: 14.7e} {self.unitWeight: 14.7e} {self.procId: 5d}"
+        return lheformat.procinfo.format(
+            xSection=self.xSection,
+            error=self.error,
+            unitWeight=self.unitWeight,
+            procId=self.procId,
+        )
 
     @classmethod
     def fromstring(cls, string: str) -> "LHEProcInfo":
@@ -432,7 +496,7 @@ class LHEInitRWGT:
         """Return a dictionary mapping weight indices to weight IDs for all weights in the <initrwgt> block."""
         return {i: w.id for i, w in enumerate(self.iter_weights())}
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:  # noqa: ARG002
         """
         Return the init block as a string in LHE format.
 
@@ -476,13 +540,13 @@ class LHEHeader:
         """Return all the attributes of the header element"""
         return {**self.extra_attributes}
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """Return the header block as a string in LHE format."""
         root = ET.Element("header", attrib=self.attributes)
         for element in self.extra_elements:
             root.append(_copy_xml_element(element))
         if self.initrwgt.entries:
-            root.append(ET.fromstring(self.initrwgt.tolhe()))
+            root.append(ET.fromstring(self.initrwgt.tolhe(lheformat=lheformat)))
 
         _indent(root)
         return ET.tostring(root, encoding="unicode", method="xml")
@@ -582,7 +646,7 @@ class LHEGenerator:
         self.extra_attributes.pop("name", None)
         self.extra_attributes.pop("version", None)
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:  # noqa: ARG002
         """
         Return the generator information as a string in LHE format.
 
@@ -604,7 +668,7 @@ class LHEInit:
     generators: list[LHEGenerator]
     """Generator information"""
 
-    def tolhe(self) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """
         Return the init block as a string in LHE format.
 
@@ -613,11 +677,11 @@ class LHEInit:
         """
         return (
             "<init>\n"
-            + self.initInfo.tolhe()
+            + self.initInfo.tolhe(lheformat=lheformat)
             + "\n"
             + "\n".join(
-                [p.tolhe() for p in self.procInfo]
-                + [g.tolhe() for g in self.generators]
+                [p.tolhe(lheformat=lheformat) for p in self.procInfo]
+                + [g.tolhe(lheformat=lheformat) for g in self.generators]
             )
             + "\n"
             + "</init>"
@@ -691,24 +755,23 @@ class LHEEvent:
         for p in self.particles:
             p.event = self
 
-    def tolhe(self, format: LHEFormat = LHEFormat.RWGT) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """
         Return the event as a string in LHE format.
 
         Args:
-            format (LHEFormat): How to serialize event weights (RWGT, WEIGHTS,
-                or NONE), see the `LHEFormat Enum`.
+            format (LHEOutputFormat): How to serialize event.
 
         Returns:
             str: The event as a string in LHE format.
         """
         sweights = ""
-        if format is LHEFormat.RWGT and self.weights:
+        if lheformat.weights is LHEWeightFormat.RWGT and self.weights:
             sweights = "<rwgt>\n"
             for k, v in self.weights.items():
                 sweights += f" <wgt id='{k}'>{v:11.4e}</wgt>\n"
             sweights += "</rwgt>\n"
-        elif format is LHEFormat.WEIGHTS and self.weights:
+        elif lheformat.weights is LHEWeightFormat.WEIGHTS and self.weights:
             sweights = "<weights>\n"
             for v in self.weights.values():
                 sweights += f"{v:11.4e}\n"
@@ -729,9 +792,9 @@ class LHEEvent:
         return (
             _open_xml_tag("event", self.attributes)
             + "\n"
-            + self.eventinfo.tolhe()
+            + self.eventinfo.tolhe(lheformat=lheformat)
             + "\n"
-            + "\n".join([p.tolhe() for p in self.particles])
+            + "\n".join([p.tolhe(lheformat=lheformat) for p in self.particles])
             + "\n"
             + soptional
             + sweights
@@ -926,7 +989,7 @@ class LesHouchesEvents:
         return {**self.extra_attributes, **attrs}
 
     def write(
-        self, output_stream: TWriteable, format: LHEFormat = LHEFormat.RWGT
+        self, output_stream: TWriteable, lheformat: LHEOutputFormat = default_format
     ) -> TWriteable:
         """
         Write the LHE file to an output stream.
@@ -935,24 +998,23 @@ class LesHouchesEvents:
         if self.comment is not None:
             output_stream.write(f"<!-- {self.comment} -->\n")
         if self.header is not None:
-            output_stream.write(self.header.tolhe() + "\n")
-        output_stream.write(self.init.tolhe() + "\n")
+            output_stream.write(self.header.tolhe(lheformat=lheformat) + "\n")
+        output_stream.write(self.init.tolhe(lheformat=lheformat) + "\n")
         for e in self.events:
-            output_stream.write(e.tolhe(format=format) + "\n")
+            output_stream.write(e.tolhe(lheformat=lheformat) + "\n")
         output_stream.write("</LesHouchesEvents>")
         return output_stream
 
-    def tolhe(self, format: LHEFormat = LHEFormat.RWGT) -> str:
+    def tolhe(self, lheformat: LHEOutputFormat = default_format) -> str:
         """
         Return the LHE file as a string.
         """
-        return self.write(io.StringIO(), format=format).getvalue()
+        return self.write(io.StringIO(), lheformat=lheformat).getvalue()
 
     def tofile(
         self,
         filepath: PathLike,
-        gz: bool = False,
-        format: LHEFormat = LHEFormat.RWGT,
+        lheformat: LHEOutputFormat = default_format,
     ) -> None:
         """
         Write the LHE file.
@@ -960,11 +1022,11 @@ class LesHouchesEvents:
         Args:
             filepath: Path to the output file.
             gz: Whether to gzip the output file (ignored if filepath suffix is .gz/.gzip).
-            format: How to serialize event weights (RWGT, WEIGHTS, or NONE), see the `LHEFormat Enum`.
+            format: How to serialize event weights (RWGT, WEIGHTS, or NONE), see the `LHEOutputFormat Enum`.
         """
         # if filepath suffix is gz, write as gz
-        with _open_write_file(filepath, gz=gz) as f:
-            self.write(f, format=format)
+        with _open_write_file(filepath, lheformat=lheformat) as f:
+            self.write(f, lheformat=lheformat)
 
     @classmethod
     def fromstring(
@@ -1141,9 +1203,11 @@ def _extract_fileobj(filepath: PathLike) -> Union[io.BufferedReader, gzip.GzipFi
     )
 
 
-def _open_write_file(filepath: PathLike, gz: bool = False) -> TextIO:
+def _open_write_file(
+    filepath: PathLike, lheformat: LHEOutputFormat = default_format
+) -> TextIO:
     filepath_str = os.fsdecode(os.fspath(filepath))
-    if filepath_str.endswith((".gz", ".gzip")) or gz:
+    if filepath_str.endswith((".gz", ".gzip")) or lheformat.file == LHEFileFormat.GZIP:
         return gzip.open(filepath_str, "wt")
     return open(filepath_str, "w")
 
