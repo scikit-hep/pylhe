b254895cb82f43700cc94b6a7c672668dda3a4c9
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2026-09-08 12:29:30 +0200

    gnerator and comment metadata

--- src/pylhe/__init__.py
+++ src/pylhe/__init__.py
@@ -1142,6 +1142,7 @@ class LesHouchesEvents:
 
         if isinstance(fileobject, h5py.File):
             init = lheh5.read_init(fileobject)
+            comment = lheh5.read_comment(fileobject)
 
             def _hdf5_generator() -> Iterator[LHEEvent]:
                 with fileobject as h5:
@@ -1150,6 +1151,7 @@ class LesHouchesEvents:
             events = _hdf5_generator()
             return LesHouchesEvents(
                 init=init,
+                comment=comment,
                 events=events if generator else list(events),
                 version=None,  # We leave the version as None since HDF5 versioning is unrelated to LHE XML versioning.
             )
